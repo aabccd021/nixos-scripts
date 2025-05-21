0612368d123418f6cc4ebc8e4b01aefe282be738
aabccd021 <aabccd021@gmail.com>
2025-05-21 20:46:19 +0700

    Refactor shell script structure and update dependencies.

--- default.nix
+++ default.nix
@@ -1,20 +1,41 @@
 { pkgs }:
 let
   scriptDefs = {
-    nixos-rebuild-local = [ pkgs.sops pkgs.openssh ];
-    nixos-rebuild-remote = [ pkgs.sops pkgs.openssh pkgs.rsync ];
-    nixos-ssh = [ pkgs.sops pkgs.openssh ];
-    nixos-rsync-push = [ pkgs.sops pkgs.openssh pkgs.rsync ];
-    nixos-rsync-pull = [ pkgs.sops pkgs.openssh pkgs.rsync ];
-    nixos-init-server = [ pkgs.sops pkgs.openssh ];
+    nixos-rebuild-local = [
+      pkgs.sops
+      pkgs.openssh
+    ];
+    nixos-rebuild-remote = [
+      pkgs.sops
+      pkgs.openssh
+      pkgs.rsync
+    ];
+    nixos-ssh = [
+      pkgs.sops
+      pkgs.openssh
+    ];
+    nixos-rsync-push = [
+      pkgs.sops
+      pkgs.openssh
+      pkgs.rsync
+    ];
+    nixos-rsync-pull = [
+      pkgs.sops
+      pkgs.openssh
+      pkgs.rsync
+    ];
+    nixos-init-server = [
+      pkgs.sops
+      pkgs.openssh
+    ];
   };
 in
 
-builtins.mapAttrs
-  (name: runtimeInputs: pkgs.writeShellApplication {
+builtins.mapAttrs (
+  name: runtimeInputs:
+  pkgs.writeShellApplication {
     name = name;
     runtimeInputs = runtimeInputs;
     text = builtins.readFile "${./scripts}/${name}.sh";
-  })
-  scriptDefs
-
+  }
+) scriptDefs
--- flake.nix
+++ flake.nix
@@ -5,7 +5,12 @@
   inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, treefmt-nix, }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      treefmt-nix,
+    }:
     let
 
       overlay = (_: prev: import ./default.nix { pkgs = prev; });
@@ -14,11 +19,14 @@
 
       treefmtEval = treefmt-nix.lib.evalModule pkgs {
         projectRootFile = "flake.nix";
-        programs.nixpkgs-fmt.enable = true;
+        programs.nixfmt.enable = true;
         programs.prettier.enable = true;
         programs.shfmt.enable = true;
         programs.shellcheck.enable = true;
-        settings.formatter.shellcheck.options = [ "-s" "sh" ];
+        settings.formatter.shellcheck.options = [
+          "-s"
+          "sh"
+        ];
         settings.global.excludes = [ "LICENSE" ];
       };
 
@@ -30,10 +38,13 @@
         buildInputs = [ pkgs.nixd ];
       };
 
-      packages = scripts // devShells // {
-        formatting = treefmtEval.config.build.check self;
-        formatter = formatter;
-      };
+      packages =
+        scripts
+        // devShells
+        // {
+          formatting = treefmtEval.config.build.check self;
+          formatter = formatter;
+        };
 
     in
     {
