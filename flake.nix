c8b7d62eab47382eb6ed6c79042e4b64c54ce2f4
aabccd021 <aabccd021@gmail.com>
2025-05-21 20:58:07 +0700

    fix: adjust nixfmt.strict setting and devShells.default in flake.nix

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
       treefmtEval = treefmt-nix.lib.evalModule pkgs {
         projectRootFile = "flake.nix";
         programs.nixfmt.enable = true;
+        programs.nixfmt.strict = true;
         programs.prettier.enable = true;
         programs.shfmt.enable = true;
         programs.shellcheck.enable = true;
@@ -34,9 +35,7 @@
 
       scripts = import ./default.nix { pkgs = pkgs; };
 
-      devShells.default = pkgs.mkShellNoCC {
-        buildInputs = [ pkgs.nixd ];
-      };
+      devShells.default = pkgs.mkShellNoCC { buildInputs = [ pkgs.nixd ]; };
 
       packages =
         scripts
