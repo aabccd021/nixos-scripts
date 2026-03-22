7e9b9200e9c88bb49ba88392fc02f86023ffe6ba
aabccd021 <aabccd021@gmail.com>
2026-03-22 13:27:34 +0700

    Add coreutils and git to runtime inputs

--- default.nix
+++ default.nix
@@ -3,33 +3,44 @@ let
   scriptDefs = {
     nixos-rebuild-local = [
       pkgs.age
+      pkgs.coreutils
       pkgs.openssh
     ];
     nixos-rebuild-remote = [
       pkgs.age
+      pkgs.coreutils
+      pkgs.git
       pkgs.openssh
       pkgs.rsync
     ];
     nixos-ssh = [
       pkgs.age
+      pkgs.coreutils
       pkgs.openssh
     ];
     nixos-mosh = [
       pkgs.age
+      pkgs.coreutils
       pkgs.mosh
     ];
     nixos-rsync-push = [
       pkgs.age
+      pkgs.coreutils
+      pkgs.git
       pkgs.openssh
       pkgs.rsync
     ];
     nixos-rsync-pull = [
       pkgs.age
+      pkgs.coreutils
+      pkgs.git
       pkgs.openssh
       pkgs.rsync
     ];
     nixos-init-server = [
       pkgs.age
+      pkgs.coreutils
+      pkgs.git
       pkgs.openssh
     ];
   };
